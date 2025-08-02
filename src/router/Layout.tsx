ef63cb0c948c45a29a5f9787ffb59fa84ea17a2a
six-standard <office@stdd.dev>
2025-08-02 17:25:04 +0900

    feature: 기본 레이아웃 설정

---
+++ src/router/Layout.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom"
+
+export const Layout = () => {
+  return <div className="w-full h-screen overflow-hidden">
+    <Outlet />
+  </div>
+}
---
+++ src/router/index.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import * as _ from "../pages";
+import { Layout } from "./Layout";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<_.Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
